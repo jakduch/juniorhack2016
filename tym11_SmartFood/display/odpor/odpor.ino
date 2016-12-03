91300a261515eefd1c1424bd0e646646e6dc7fbd
Jakub Duchek <jakduch@seznam.cz>
2016-12-03 06:50:22 +0100

    Automat: 	- Vypisuje data předaná z PC 	- PC umí zjistit jestli je připojený automat
    	- Připojení k databázi
    
    Mobilní aplikace:
    	- Přihlašování
    	- Napojení na MySQL
    	- Rozdílná obrazovka při přihlášení
    
    
    Webová aplikace:
    	- Seznam produktů, úprava, cena
    	- Seznam automatů (lokace, název, úprava)
    	- Úpravy uživatelů včetně oprávnění
    	- Koupě kreditů
    Konec chatu

--- tym11_SmartFood/display/odpor/odpor.ino
+++ tym11_SmartFood/display/odpor/odpor.ino
@@ -1,8 +1,24 @@
+#define cit 500
+#define s 500
+#define l 1000
 void setup() {
- Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println();
-  digitalRead(7);
+  if (analogRead() > cit) {
+    delay(s);
+    if (analogRead() < 750) {
+      delay(s);
+      if (analogRead() > cit) {
+        delay(s);
+        if (analogRead() < 500) {
+
+        }
+      }
+    }
+  }
+
+
+  Serial.println(analogRead(0));
 }
